12c8f0117fab8d9592db0afc5e74a9a6a2aff7d9
DisconnectedW <238344850+DisconnectedW@users.noreply.github.com>
2025-10-17 21:00:47 +0100

    The javascript has been updated The logic to play a single round has been implemented

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     let index = Math.floor(Math.random() * 3);
@@ -6,6 +9,30 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     const userInput = prompt("Rock, Paper or Scissors");
-    return userInput.toLowerCase();
+    return userInput;
 }
 
+function playRound(humanChoice,computerChoice) {
+    const humanInput = humanChoice.toLowerCase();
+    if (humanInput === computerChoice) {
+        console.log("Its a draw, nobody gets a point")
+    } else if (humanInput === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats Scissors");
+        humanScore++;
+    } else if (humanInput === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats Rock");
+        humanScore++;
+    } else if (humanInput === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats Paper");
+        humanScore++;
+    } else if (computerChoice === "paper" && humanInput === "rock") {
+        console.log("You lose! Paper beats Rock");
+        computerScore++;
+    } else if (computerChoice === "rock" && humanInput === "scissors") {
+        console.log("You lose! Rock beats Scissors");
+        computerScore++;
+    } else if (computerChoice === "scissors" && humanInput === "paper") {
+        console.log("You lose! Scissors beats Paper");
+        computerScore++;
+    }
+}
